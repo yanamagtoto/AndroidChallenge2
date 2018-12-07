ade2c24879a1ac1bc098f4e549faf134d5ccd86b
Aliana <aliana_magtoto@dlsu.edu.ph>
2018-12-07 10:45:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/andres/androidchallenge2/Database.java
@@ -0,0 +1,25 @@
+package com.example.andres.androidchallenge2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper {
+
+    public static final String database_name = "conversiondb.db";
+    
+
+    public Database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
